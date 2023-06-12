92a5f87d623b02c36cb9213aabb22b7e0462c67f
cezarystepkowski <cezary.stepkowski@protonmail.com>
2023-06-12 23:14:41 +0200

    Load product data based on environment

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly PUBLIC_APP_ENV: 'prod' | 'staging' | 'dev';
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
