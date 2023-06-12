c006ae12acf7431244ef6788cc3c4e62e43364ba
cezarystepkowski <cezary.stepkowski@protonmail.com>
2023-06-12 23:14:41 +0200

    Load product data based on environment

--- src/shippingRateRepository.ts
+++ src/shippingRateRepository.ts
@@ -1,10 +1,25 @@
-import productsData from './data/shipping_rates.dist.json';
+import shippingRatesDataProd from './data/shipping_rates.prod.json';
+import shippingRatesDataStaging from './data/shipping_rates.staging.json';
+import shippingRatesDataDev from './data/shipping_rates.dev.json';
 
 interface ShippingRate {
     id: string;
 }
 
-const shippingRates: ShippingRate[] = productsData as ShippingRate[];
+function getShippingRatesData(): ShippingRate[] {
+    switch (import.meta.env.PUBLIC_APP_ENV) {
+        case 'prod':
+            return shippingRatesDataProd;
+        case 'staging':
+            return shippingRatesDataStaging;
+        case 'dev':
+            return shippingRatesDataDev;
+        default:
+            throw new Error(`Invalid PUBLIC_APP_ENV "${import.meta.env.PUBLIC_APP_ENV}"`);
+    }
+}
+
+const shippingRates: ShippingRate[] = getShippingRatesData();
 
 export function allEnabled(): ShippingRate[] {
     return shippingRates;
